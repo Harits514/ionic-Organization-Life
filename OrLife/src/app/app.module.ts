94bf2ebbc21550397519794068013ffce45a20cc
Harits514 <m.harits7@gmail.com>
2018-11-22 08:38:04 +0700

    udah bisa konek ke api(async)

--- OrLife/src/app/app.module.ts
+++ OrLife/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/a	bout';
+import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
@@ -13,6 +14,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { NgxQRCodeModule } from 'ngx-qrcode2';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { Camera } from '@ionic-native/camera';
+import { ApiProvider } from '../providers/api/api';
 
 
 @NgModule({
@@ -26,7 +28,8 @@ import { Camera } from '@ionic-native/camera';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    NgxQRCodeModule
+    NgxQRCodeModule,
+     HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -41,7 +44,8 @@ import { Camera } from '@ionic-native/camera';
     SplashScreen,
     Camera,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    BarcodeScanner
+    BarcodeScanner,
+    ApiProvider
   ]
 })
 export class AppModule {}
---
+++ OrLife/src/providers/api/api.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map,catchError } from "rxjs/operators";
+
+
+/*
+  Generated class for the ApiProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class ApiProvider {
+
+  constructor(public http: HttpClient) {
+    console.log('Hello ApiProvider Provider');
+  }
+  
+  getFilms() {
+      this.http.get('https://swapi.co/api/people/1')
+      .subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  
+  }
+
+}
