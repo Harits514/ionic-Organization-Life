12a0eb838c9a9e5154d993281e8921d1cae767e4
DarkBoyOKI <okimonoki@rocketmail.com>
2018-12-06 03:06:18 +0700

    FullcoderMagician
    Ho Ho Ho

--- OrLife/src/pages/contact/contact.ts
+++ OrLife/src/pages/contact/contact.ts
@@ -1,5 +1,9 @@
+import { ApiProvider } from './../../providers/api/api';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { Observable } from 'rxjs/Observable';
+ 
 
 @Component({
   selector: 'page-contact',
@@ -7,8 +11,34 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
-
+  qrData = null;
+  createdCode : null;
+  ay = null;
+  cobs : null;
+  scannedCode = null;
+ 
+  constructor(private barcodeScanner: BarcodeScanner, public apiProvider: ApiProvider, navCtrl: NavController) { 
+  }
+ 
+  createCode() {
+    this.createdCode = this.apiProvider.getFilms()
+    .then(data => {
+      this.createdCode = data;
+      console.log(this.createdCode.name);
+    });
+    console.log(this.createdCode.name)
+  }
+ 
+  scanCode() {
+    this.barcodeScanner.scan().then(barcodeData => {
+      this.scannedCode = barcodeData.text;
+    }, (err) => {
+        console.log('Error: ', err);
+    });
+  }
+  
+  logOut(): void {
+    this.navCtrl.setRoot("LoginPage");
   }
 
 }
